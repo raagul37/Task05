9a03e646c16f81005f6b8b6a186248ec4e4e43ce
raagul37 <87649564+raagul37@users.noreply.github.com>
2021-07-30 15:33:01 +0530

    Add files via upload

---
+++ mrfconcepts.js
@@ -0,0 +1,22 @@
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    console.log(data);
+var result=data.filter((element)=>element.region==='Asia');
+console.log(result);
+
+var result1=data.filter((element)=>element.population<200000);
+console.log(result1);
+var result2=data.filter((element)=>{
+    for(let i in element.currencies){
+        if(element.currencies[i].code==='USD'){
+            return true;
+        }
+    }
+});
+console.log(result2);
+var result3=data.reduce((acc,element)=>acc+element.population,0);
+console.log(result3);
+}
